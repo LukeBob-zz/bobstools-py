ef956b228e4c27eada12b7cb246972d81c657a2a
Luke <bob_monday@outlook.com>
2016-10-28 23:51:46 +0100

    Create PingBot.py

---
+++ PingBot.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+#
+# Author: LukeBob
+#
+#
+import os
+import time
+from twilio.rest import TwilioRestClient
+import sys
+
+ACCSID = 'AC'  # Account SID
+AUTHTOK = ''   # Auth Token
+
+
+while 1:
+    time.sleep(20)
+    response = os.system('ping -c 1 <Your server> >> /dev/null')
+    time.sleep(5)
+    if response != 0:
+        client = TwilioRestClient(ACCSID, AUTHTOK)
+        client.messages.create( body="Server is Down", to="<Mobile number>", from_="<Twilio Number>")
+        time.sleep(10)
+        sys.exit(0)
+    
+
