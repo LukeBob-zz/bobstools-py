9fac419c9b71851e1fcb7a8484ae25d705ff90c9
Luke <bob_monday@outlook.com>
2016-10-29 00:30:18 +0100

    Update PingBot.py

--- PingBot.py
+++ PingBot.py
@@ -2,7 +2,11 @@
 #
 # Author: LukeBob
 #
-# Run with: nohup python PingBot.py &
+# Can be run on both windows and linux systems.
+#
+# Windows: PingBot.py , however on windows you will need the cmd prompt open at all times to keep the script running.
+#
+# Linux: Run with: nohup python PingBot.py &
 
 
 import os
@@ -12,16 +16,28 @@ import sys
 
 ACCSID = 'AC'  # Account SID
 AUTHTOK = ''   # Auth Token
+name = os.name
 
 
-while 1:
-    time.sleep(20)
-    response = os.system('ping -c 1 <Your server> >> /dev/null')
-    time.sleep(5)
-    if response != 0:
-        client = TwilioRestClient(ACCSID, AUTHTOK)
-        client.messages.create( body="Server is Down", to="<Mobile number>", from_="<Twilio Number>")
-        time.sleep(10)
-        sys.exit(0)
-    
+if name == 'nt':
+    while 1:
+        time.sleep(20)
+        response = os.system('ping -n 1 <Server Name> 2>&1>nul')
+        time.sleep(5)
+        if response != 0:
+            client = TwilioRestClient(ACCSID, AUTHTOK)
+            client.messages.create( body="Server is Down", to="<Your Phone Num>", from_="<Twilio Phone Num>")
+            time.sleep(10)
+            sys.exit(0)
+
+else:
+    while 1:
+        time.sleep(20)
+        response = os.system('ping -n 1 <Server Name> 2>&1>nul')
+        time.sleep(5)
+        if response != 0:
+            client = TwilioRestClient(ACCSID, AUTHTOK)
+            client.messages.create( body="Server is Down", to="<Your Phone Num>", from_="<Twilio Phone Num>")
+            time.sleep(10)
+            sys.exit(0)
 
