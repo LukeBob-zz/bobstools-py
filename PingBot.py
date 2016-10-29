a26591797999d17f06785e2a9a45b295e7c08b8e
Luke <bob_monday@outlook.com>
2016-10-29 01:09:12 +0100

    Update PingBot.py

--- PingBot.py
+++ PingBot.py
@@ -36,26 +36,32 @@ else:
     print(Linux)
 
 
+try:
+    if name == 'nt':
+        while 1:
+            time.sleep(20)
+            response = os.system('ping -n 1 <Server Name> 2>&1>nul')
+            time.sleep(5)
+            if response != 0:
+                client = TwilioRestClient(ACCSID, AUTHTOK)
+                client.messages.create( body="Server is Down", to="<Your Phone Num>", from_="<Twilio Phone Num>")
+                time.sleep(10)
+                sys.exit(0)
 
-if name == 'nt':
-    while 1:
-        time.sleep(20)
-        response = os.system('ping -n 1 <Server Name> 2>&1>nul')
-        time.sleep(5)
-        if response != 0:
-            client = TwilioRestClient(ACCSID, AUTHTOK)
-            client.messages.create( body="Server is Down", to="<Your Phone Num>", from_="<Twilio Phone Num>")
-            time.sleep(10)
-            sys.exit(0)
+    else:
+        while 1:
+            time.sleep(20)
+            response = os.system('ping -n 1 <Server Name> 2>&1>nul')
+            time.sleep(5)
+            if response != 0:
+                client = TwilioRestClient(ACCSID, AUTHTOK)
+                client.messages.create( body="Server is Down", to="<Your Phone Num>", from_="<Twilio Phone Num>")
+                time.sleep(10)
+                sys.exit(0)
 
-else:
-    while 1:
-        time.sleep(20)
-        response = os.system('ping -n 1 <Server Name> 2>&1>nul')
-        time.sleep(5)
-        if response != 0:
-            client = TwilioRestClient(ACCSID, AUTHTOK)
-            client.messages.create( body="Server is Down", to="<Your Phone Num>", from_="<Twilio Phone Num>")
-            time.sleep(10)
-            sys.exit(0)
+except KeyboardInterrupt:
+    print ("Exiting...")
+except Exception:
+    traceback.print_exc(file=sys.stdout)
+sys.exit(0)
 
