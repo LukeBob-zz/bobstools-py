004ac9a56042862bbd7b929208c1ef2825a02f28
Luke <bob_monday@outlook.com>
2016-11-04 17:24:22 +0000

    Update BobDir.py

--- BobDir.py
+++ BobDir.py
@@ -25,8 +25,6 @@ class colors:
     UNDERLINE = '\033[4m'
     OKBLUE = '\033[94m'
 
-list1 = []
-list2 = []
 MAX_THREADS = 20
 subprocess.call('clear', shell=True)
 smallSplit = colors.BOLD + colors.OKBLUE + '============================' + colors.ENDC
