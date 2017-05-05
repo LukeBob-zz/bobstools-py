3147416b7101e8b50296e4ad4b9cc7a3ae05494b
Luke <bob_monday@outlook.com>
2017-05-05 23:26:56 +0100

    Update LogCleaner.py

--- LogCleaner.py
+++ LogCleaner.py
@@ -7,7 +7,6 @@ dur_list = []
 file_list = []
 
 def removal(file_path):
-
     if file_path.endswith('.gz'):
         try:
             os.system('rm {}'.format(file_path))
@@ -15,24 +14,24 @@ def removal(file_path):
         except:
             raise
 
-def list_files(startpath):
+def list_files(startpath):    
     for root, dirs, files in os.walk(startpath):
         level = root.replace(startpath, '').count(os.sep)
         for f in files:
-
             if f.endswith('.gz'):
                 dur = os.path.basename(root)
                 filez = f
                 dur_list.append(dur)
                 file_list.append(f)
 
-def main():
+def main():    
     a=0
     list_files('/var/log/')
     for file in file_list:
         a=a+1
-
+        
     print('Found: {} Files\n'.format(str(a)))
+    
     if a > 1:
         for b, c in zip(dur_list, file_list):
             file_path = '/var/log/'+b+'/'+c
