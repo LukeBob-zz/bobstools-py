f70b71a28ab0cb0d39eec82ef456f890325c9e54
Luke <bob_monday@outlook.com>
2017-11-30 23:14:34 +0000

    Update linkShrink.py

--- linkShrink.py
+++ linkShrink.py
@@ -5,7 +5,8 @@
 # Requires pyshorteners library.
 # Usage: ./linkShrink -u <url> -m <module>
 # if your not sure about the modules, --module-list will give you a list of modules available, -h or --help for help
-                                                                       
+ 
+import requests
 from pyshorteners import Shortener
 import sys
 import argparse
